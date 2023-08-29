6dc402be608538f06e69de4d686b218bb2341652
Bailey Bell Loffredo <baileybell.loffredo@gmail.com>
2023-08-29 12:02:03 -0400

    finished

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,19 +1,44 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
 
 function PokemonPage() {
+  const [pokeData, setPokemon] = useState([])
+  const [filteredPokeData, setFilterPokeData] = useState([])
+
+
+  const url = "http://localhost:3001/pokemon"
+
+  useEffect(() => {
+    fetch(url)
+    .then(resp => resp.json())
+    .then((data) => {
+      setPokemon(data);
+      setFilterPokeData(data);
+    })}, [])
+
+    const updateSearch= (search) => {
+      const filteredPoke = pokeData.filter(poke => poke.name.toLowerCase().includes(search.toLowerCase()))
+      setFilterPokeData(filteredPoke)
+    }
+  
+
+    function newPokemons(newPokemon){
+      setPokemon([...pokeData, newPokemon])
+    }
+
+
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
       <br />
-      <PokemonForm />
+      <PokemonForm newPokemons={newPokemons} />
       <br />
-      <Search />
+      <Search updateSearch={updateSearch}/>
       <br />
-      <PokemonCollection />
+      <PokemonCollection pokeData={filteredPokeData}/>
     </Container>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 
-function Search() {
+function Search({updateSearch}) {
+
+  const [search, setSearch] = useState("")
+
+
+  const handleChange = (e) => {
+    setSearch(e.target.value)
+    updateSearch(e.target.value)
+    ;};
+
+
   return (
     <div className="ui search">
       <div className="ui icon input">
-        <input className="prompt" />
+        <input  value={search} className="prompt" onChange={handleChange} />
         <i className="search icon" />
       </div>
     </div>
